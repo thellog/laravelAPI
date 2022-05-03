eefb00f46d257e58d8bb99354c61a2b0f865d25e
thellog <firaxaxa@gmail.com>
2022-05-03 12:07:57 +0700

    upload image to cloudinary

--- app/Http/Services/MediaService.php
+++ app/Http/Services/MediaService.php
@@ -3,8 +3,29 @@ namespace App\Http\Services;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Exception;
 
 class MediaService {
     public function uploadImg($request) {
+        $data = Cloudinary::uploadApi()->upload($request->file('avatar')->getRealPath());
+
+        $public_id = $data['public_id'];
+        $url       = $data['url'];
+
+        try {
+            User::where('id', $request->id)
+                    ->update([
+                        'public_id' => $public_id,
+                        'avatar_url' => $url
+                    ]);
+            return array([
+                'id' => $request->id,
+                'public_id' => $public_id,
+                'avatar_url' => $url       
+            ]);
+        }catch(Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
