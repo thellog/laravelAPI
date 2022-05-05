46b8659daaf263a4569031c3375ba6df5568fd70
linhtql <linhdz.na@gmail.com>
2022-05-05 20:14:56 +0700

    get video lists

--- config/services.php
+++ config/services.php
@@ -30,4 +30,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'youtube' => [
+        'api_key' => env('API_KEY'),
+        'search_endpoint' => env('SEARCH_ENDPOINT'),
+        'channel' => env('YOUTUBE_ID')
+    ]
+
 ];
